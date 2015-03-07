4887fa30afc577516c5dd5d4538f449a46f0f828
Prateep Kul <1.0@kul.com>
2015-03-08 01:33:35 +0700

    Add artcile blogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  resources :articles do
+    resources :comments
+  end
   resources :agendas
 
   resources :projects
-  root :to => 'projects#index'
+    root :to => 'projects#index'
   #root 'welcome#index'
+
 end
+
